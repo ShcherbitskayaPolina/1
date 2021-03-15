bd1522506d90a4c25d0c9bd06d072c065a7f0eb2
SHCHERBITSKAYA <serbickaap9@gmail.com>
2021-03-15 09:55:17 +0300

    2

---
+++ 1/Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    class Library
+    {
+        public string Name { get; set; }
+        public string Adres { get; set; }
+        public int God { get; set; }
+        public Library()
+        {
+            Name = null;
+            Adres = null;
+            God = 0;
+        }
+
+    }
+}
